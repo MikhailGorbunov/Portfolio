e1232eed075d5146fa8e600ad2bc5f26a5449416
Mikhail Gorbunov <mikle.gorbunov@gmail.com>
2023-11-10 14:46:12 +0000

    Made it mobile responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,23 +6,24 @@ function Footer() {
   return (
     <footer
       className="w-full border-t-2 border-solid border-dark font-medium text-lg
-    dark: text-light dark:border-light"
+    dark: text-dark dark:text-light dark:border-light sm:text-base"
     >
-      <Layout className="py-8 flex items-center justify-between">
+      <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-        <div className="flex items-center">
-          Build With{" "}
+        <div className="flex items-center lg:py-2">
+          Build With
           <span className="text-primary dark:text-primaryDark text-2xl px-1">
             &#9825;
           </span>
+          by&nbsp;
+          <Link
+            href="https://yourWeb.com"
+            target={"_blank"}
+            className="underline underline-offset-2"
+          >
+            CodeBucks
+          </Link>
         </div>
-        by&nbsp;
-        <Link
-          href="https://yourWeb.com"
-          className="underline underline-offset-2"
-        >
-          CodeBucks
-        </Link>
         <Link
           href="https://yourWeb.com"
           target={"_blank"}
